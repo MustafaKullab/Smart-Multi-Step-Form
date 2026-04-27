a4998a5d2e550ed0bb5dad24446107cea780744d
Mustafa Kullab <momoana013@gmail.com>
2026-04-27 21:42:12 +0300

    finish responsive design

--- src/components/SideBar.vue
+++ src/components/SideBar.vue
@@ -2,28 +2,36 @@
   <div class="SideBar">
     <div class="row ms-3">
       <div class="steps col-md-10">
-        <div class="step1 mt-4 d-flex align-items-center gap-4 mb-3">
+        <div
+          class="step1 mt-4 d-flex flex-column flex-lg-row text-center text-lg-start align-items-center gap-4 mb-3"
+        >
           <div class="stepNum text-light active">1</div>
           <div class="stepText">
             <p class="stepCount m-0 text-uppercase">step 1</p>
             <span class="title text-uppercase text-light fw-bold">your info</span>
           </div>
         </div>
-        <div class="step2 mt-4 d-flex align-items-center gap-4 mb-3">
+        <div
+          class="step2 mt-4 d-flex flex-column flex-lg-row text-center text-lg-start align-items-center gap-4 mb-3"
+        >
           <div class="stepNum text-light">2</div>
           <div class="stepText">
             <p class="stepCount m-0 text-uppercase">step 2</p>
             <span class="title text-uppercase text-light fw-bold">select plan</span>
           </div>
         </div>
-        <div class="step3 mt-4 d-flex align-items-center gap-4 mb-3">
+        <div
+          class="step3 mt-4 d-flex flex-column flex-lg-row text-center text-lg-start align-items-center gap-4 mb-3"
+        >
           <div class="stepNum text-light">3</div>
           <div class="stepText">
             <p class="stepCount m-0 text-uppercase">step 3</p>
             <span class="title text-uppercase text-light fw-bold">add-ons</span>
           </div>
         </div>
-        <div class="step4 mt-4 d-flex align-items-center gap-4 mb-3">
+        <div
+          class="step4 mt-4 d-flex flex-column flex-lg-row text-center text-lg-start align-items-center gap-4 mb-3"
+        >
           <div class="stepNum text-light">4</div>
           <div class="stepText">
             <p class="stepCount m-0 text-uppercase">step 4</p>
@@ -39,27 +47,44 @@
 .SideBar {
   height: 77vh;
   background-image: url("../public/images/bg-sidebar-desktop.svg");
-
   background-repeat: no-repeat;
-  .steps {
-    > div {
-      .stepNum {
-        width: 40px;
-        height: 40px;
+  overflow: hidden;
+  @media (max-width: 767px) {
+    background-image: url("../public/images/bg-sidebar-mobile.svg") !important;
+    background-size: 100%;
+    height: 21vh;
+  }
+  .row {
+    .steps {
+      @media (max-width: 767px) {
         display: flex;
-        align-items: center;
-        justify-content: center;
-        border-radius: 50%;
-        border: 2px solid #bee1ff;
-        &.active {
-          background: #bee1ff !important;
-          color: #0e2e5d !important;
-          font-weight: bold;
-        }
+        justify-content: center !important;
+        align-items: center !important;
+        margin-top: 20px;
+        gap: 20px;
       }
-      .stepText {
-        .stepCount {
-          color: #bee1ff;
+      > div {
+        .stepNum {
+          width: 40px;
+          height: 40px;
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          border-radius: 50%;
+          border: 2px solid #bee1ff;
+          &.active {
+            background: #bee1ff !important;
+            color: #0e2e5d !important;
+            font-weight: bold;
+          }
+        }
+        .stepText {
+          .stepCount {
+            color: #bee1ff;
+          }
+          @media (max-width: 767px) {
+            display: none !important;
+          }
         }
       }
     }
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="HomePage">
-    <div class="container-fluid">
-      <div class="row m-5 p-3 rounded-3 bg-white">
-        <div class="col-md-5 col-xl-4 p-0">
+    <div class="container-fluid w-75">
+      <div class="row m-5 p-3 rounded-3 bg-white justify-content-between position-relative">
+        <div class="col col-md-5 col-xl-4 p-0">
           <SideBar />
         </div>
-        <div class="col-md-7">
+        <div class="detailsSide col-md-7">
           <div class="box yourInfo">
             <div class="title mt-5">
               <h1>Personal info</h1>
@@ -35,6 +35,9 @@
                 />
               </div>
             </form>
+            <div class="nxtBtn text-end">
+              <button class="btn mt-5">Next Step</button>
+            </div>
           </div>
         </div>
       </div>
@@ -47,18 +50,84 @@ import SideBar from "@/components/SideBar.vue";
 </script>
 
 <style lang="scss" scoped>
-.box {
-  h1 {
-    color: #052852 !important;
-  }
-  .subtitle {
-    color: #a1a1a9 !important;
-  }
-  form {
-    > div {
-      label {
-        color: #052852;
-        font-weight: bold;
+.HomePage {
+  .container-fluid {
+    @media (max-width: 767px) {
+      width: 100% !important;
+      padding: 0 !important;
+      .row {
+        margin: 0 !important;
+        padding: 0 !important;
+      }
+    }
+    .row {
+      .detailsSide {
+        @media (max-width: 767px) {
+          position: absolute !important;
+          z-index: 6000 !important;
+          top: 110px;
+          background-color: white;
+          width: 80%;
+          left: 50%;
+          transform: translateX(-50%);
+          border-radius: 10px;
+          box-shadow: 0 0.125rem 0.25rem rgba(0, 0, 0, 0.075);
+        }
+        .box {
+          .title {
+            @media (max-width: 767px) {
+              margin-top: 20px !important;
+            }
+            h1 {
+              color: #052852 !important;
+            }
+            .subtitle {
+              color: #a1a1a9 !important;
+            }
+          }
+
+          form {
+            @media (max-width: 767px) {
+              margin: 20px 0 !important;
+            }
+            input {
+              box-shadow: none !important;
+              &:focus {
+                border-color: #052852 !important;
+              }
+            }
+            > div {
+              label {
+                color: #052852;
+                font-weight: bold;
+              }
+            }
+          }
+        }
+        .nxtBtn {
+          @media (max-width: 767px) {
+            display: flex;
+            justify-content: end;
+            align-items: center;
+            margin-top: 40px;
+            position: absolute;
+            right: 0;
+            width: 100%;
+            background-color: white;
+            padding: 20px 10px 20px 20px;
+            border-radius: 6px;
+          }
+          button {
+            background-color: #174a8b !important;
+            color: #f4ffff !important;
+            &:hover {
+              background-color: #144079 !important;
+            }
+            @media (max-width: 767px) {
+              margin: 0 !important;
+            }
+          }
+        }
       }
     }
   }
